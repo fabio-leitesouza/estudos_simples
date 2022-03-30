438eae6e549d4aff36202f8ff3fa111a58ea7d7c
fabio souza <fabiorobertoleite@gmail.com>
2022-03-30 08:39:22 -0300

    atualizacoes simples e novo

---
+++ var.js
@@ -0,0 +1,23 @@
+// var peso = 97
+// var altura = 1.79
+// imc = peso/(altura *altura)
+
+// var imc
+
+/*quando JavaScript for executado ele verifica todas as variáveis do tipo var e carregam antes. 
+Esta é a vantagem da car*/
+//como podemos usar o var em qualquer momento do código isso pode gerar alguns erros.
+
+// let peso = 97
+// let altura = 1.78
+// let imc = 5
+// if (true){
+//     let imc = peso / (altura*altura)
+//     console.log(imc)
+// }
+
+// const peso = 97
+// const altura = 1.78
+// const imc = 25
+// imc = peso / (altura*altura)
+// console.log(imc)
